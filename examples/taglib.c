931ece754414659e52345c00648b9eb72fc8a4fa
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2016-12-30 16:12:20 +0100

    added notes to taglib extension

--- examples/taglib.c
+++ examples/taglib.c
@@ -1,3 +1,24 @@
+/***************************************************************************
+    begin........: December 2016
+    copyright....: Sebastian Fedrau
+    email........: sebastian.fedrau@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License v3 as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License v3 for more details.
+ ***************************************************************************/
+
+/***************************************************************************
+   This extension for efind reads tags and properties from audio files.
+ ***************************************************************************/
+
 #define _GNU_SOURCE
 
 #include <stdio.h>
