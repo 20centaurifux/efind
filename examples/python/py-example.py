50cf032bae36e284ef4ea463f04801b52791569d
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2017-05-19 06:31:15 +0200

    renamed example files, test string parameter in python example code

--- examples/python/py-example.py
+++ examples/python/py-example.py
@@ -5,12 +5,17 @@ EXTENSION_VERSION="0.1.0"
 EXTENSION_DESCRIPTION="An example extension written in Python."
 
 def py_check_extension(filename, extension, icase):
+    result = 0
+
     _, ext = os.path.splitext(filename)
 
-    if icase == 0:
-        return ext == extension
-    else:
-        return ext.lower() == extension.lower()
+    if len(ext) > 0:
+        if icase == 0:
+            result = ext == extension
+        else:
+            result = ext.lower() == extension.lower()
+
+    return result
 
 py_check_extension.__signature__=[str, int]
 
