6629ab14d1efe1b0329a22ad7bd2a742a91faafb
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2017-12-01 17:33:59 +0100

    added prefix to test Python filter functions

--- test/extensions/py-test.py
+++ test/extensions/py-test.py
@@ -2,19 +2,19 @@ EXTENSION_NAME="py-test"
 EXTENSION_VERSION="0.1.0"
 EXTENSION_DESCRIPTION="efind test extension."
 
-def name_equals(filename, name):
+def py_name_equals(filename, name):
     return filename == name
 
-name_equals.__signature__=[str]
+py_name_equals.__signature__=[str]
 
-def add(filename, a, b):
+def py_add(filename, a, b):
     return a + b
 
-add.__signature__=[int, int]
+py_add.__signature__=[int, int]
 
-def sub(filename, a, b):
+def py_sub(filename, a, b):
     return a - b
 
-sub.__signature__=[int, int]
+py_sub.__signature__=[int, int]
 
-EXTENSION_EXPORT=[name_equals, add, sub]
+EXTENSION_EXPORT=[py_name_equals, py_add, py_sub]
