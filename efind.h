98aa40aca6cad52515eebcade48d139534fc15e2
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-12-23 21:28:55 +0100

    set version to 0.5.2

--- efind.h
+++ efind.h
@@ -31,7 +31,7 @@
 /*! Minor version. */
 #define EFIND_VERSION_MINOR     5
 /*! Patch level. */
-#define EFIND_VERSION_PATCH     1
+#define EFIND_VERSION_PATCH     2
 /*! Code name. */
 #define EFIND_VERSION_CODE_NAME "James Cook"
 /*! Project website. */
